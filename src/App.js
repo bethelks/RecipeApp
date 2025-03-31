6dbfbe8fd2ca12e4f062b0f7bea0303a02d556d6
Prof Hines <chines@bethelks.edu>
2025-03-31 09:29:26 -0500

    Update App.js
    Updated merge conflict for Seth

--- src/App.js
+++ src/App.js
@@ -1,16 +1,3 @@
-// import React from 'react';
-// import FilterRecipes from './FilterRecipes'; // Adjust the path if necessary
-// import './App.css'; // Ensure this file exists and includes any global styles
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <FilterRecipes />
-//     </div>
-//   );
-// }
-
-// export default App;
 import React from "react";
 import { BrowserRouter, Routes, Route }from "react-router-dom";
 import Home from "./components/Home";

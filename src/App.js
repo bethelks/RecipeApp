036400b925d3809db73847fa4366fb6ad18ae531
Balzzzz9 <sbalzer25@gmail.com>
2025-03-31 09:17:47 -0500

    Update App.js
    Manually Fixed Merge

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import React from 'react';
-import FilterRecipes from './FilterRecipes'; // Adjust the path if necessary
-import './App.css'; // Ensure this file exists and includes any global styles
+// import React from 'react';
+// import FilterRecipes from './FilterRecipes'; // Adjust the path if necessary
+// import './App.css'; // Ensure this file exists and includes any global styles
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <FilterRecipes />
+//     </div>
+//   );
+// }
+
+// export default App;
+import React from "react";
+import { BrowserRouter, Routes, Route }from "react-router-dom";
+import Home from "./components/Home";
+import TempNavigate from "./TempNavigate";
+import RecipePage from "./components/RecipePage";
+import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <FilterRecipes />
-    </div>
+    <BrowserRouter>
+      <TempNavigate />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/recipe" element={<RecipePage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 

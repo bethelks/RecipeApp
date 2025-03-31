71cebfa12e8576249bbcd94cd104187c43982fd4
sethabalzer <sethabalzer@bethelks.edu>
2025-03-31 10:15:13 -0500

    Fix: Removed submodule reference for components

---
+++ src/components/RecipePage.js
@@ -0,0 +1,13 @@
+// I am assuming this is where the filtered recipe search results will go - Seth
+
+import React from "react";
+
+function RecipePage() {
+  return (
+    <div>
+      <h1>Recipe Page</h1>
+    </div>
+  );
+}
+
+export default RecipePage;

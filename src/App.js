69aa6f20ca876c732a278be9deefb3e46dafee3b
Kennedy White <kennedyrwhite@bethelks.edu>
2025-04-03 14:20:33 -0500

    Fix issue with embedded repository and commit other changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-
 import React from "react";
-import { BrowserRouter, Routes, Route }from "react-router-dom";
-import Home from "./components/Home";
-import TempNavigate from "./TempNavigate";
-import FilterRecipes from './FilterRecipes';
-import RecipePage from "./components/RecipePage";
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"; // Use Routes instead of Switch
+import LoginScreen from "./LoginScreen"; // Ensure the import path is correct
+import ForgotPasswordScreen from "./ForgotPasswordScreen"; // Import the ForgotPasswordScreen component
 
 function App() {
   return (
-    <BrowserRouter>
-      <TempNavigate />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/recipe" element={<RecipePage />} />
-      </Routes>
-    </BrowserRouter>
-  )
+    <Router>
+      <div>
+        <header>
+          {/* Displaying the SSL certificate image */}
+          <img
+            src={`${process.env.PUBLIC_URL}/ssl_certificate.png`}
+            alt="SSL Certificate"
+            className="ssl-image"
+          />
+        </header>
+
+        {/* Define routes */}
+        <Routes>
+          {/* Route for LoginScreen */}
+          <Route path="/" element={<LoginScreen />} />
+          {/* Route for ForgotPasswordScreen */}
+          <Route path="/forgot-password" element={<ForgotPasswordScreen />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
-
